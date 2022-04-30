c37731df0791f2fd7d21d4c0980f2899d4e3e4ce
PratikChitruk <pratik.chitruk@gmail.com>
2022-04-30 12:23:48 +0530

    [Pratik] Add InventoryManagment

---
+++ InventoryManagement/InventoryManagement/Program.cs
@@ -0,0 +1,16 @@
+﻿using JsonOOPS.InventoryManagement;
+using System;
+
+namespace JsonOOPS
+{
+    class Program
+    {
+        const string INVENTORY_JSON = @"C:\Users\Admin\OneDrive\Desktop\Bridzlabs\Object oriented programming\InventoryManagement\InventoryManagement\Inventory.json";
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Json Inventory management program ");
+            InventoryMain inv = new InventoryMain();
+            inv.ShowOptions(INVENTORY_JSON);
+        }
+    }
+}
