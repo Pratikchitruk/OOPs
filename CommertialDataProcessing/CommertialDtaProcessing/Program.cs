6b995f7aff73493b1582e266bf39af7aa1bca94b
PratikChitruk <pratik.chitruk@gmail.com>
2022-04-30 12:39:39 +0530

    [Pratik] Add Commertial Data

---
+++ CommertialDataProcessing/CommertialDtaProcessing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Commercial_data_processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\Admin\OneDrive\Desktop\Bridzlabs\Object oriented programming\CommertialDataProcessing\CommertialDtaProcessing\StocksFile.json";
+
+            Console.WriteLine(" Welcome to Commercial_data_processing Stock_management Program");
+            StockMain sm = new StockMain();
+            sm.ReadJsonFile(filePath);
+        }
+    }
+}
